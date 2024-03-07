02e99cb586b84bba60d63dc84da93b9bcc9b977b
Anton Zadorozniy <zadoroshniy_anton_2003@mail.ru>
2024-03-07 16:55:27 +0300

    tetherspecialnew -> tetherspecial

---
+++ src/pages/tetherspecialold/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <landing-promo/>
+  <landing-calculation />
+  <s-site-footer/>
+</template>
+
+<script setup>
+import {definePageMeta} from "#imports";
+import SSiteFooter from "~/src/shared/ui/site/sections/s-site-footer/s-site-footer.vue";
+
+definePageMeta({
+  layout: 'site-dark',
+})
+</script>
+
+<style lang="scss">
+
+</style>
