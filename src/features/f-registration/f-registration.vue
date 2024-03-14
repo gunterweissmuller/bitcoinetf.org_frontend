265f9ddb0b52de668309ed4102d76059f7a0df50
Anton Zadorozniy <zadoroshniy_anton_2003@mail.ru>
2024-03-14 08:36:42 +0300

    telegram debug

--- src/features/f-registration/f-registration.vue
+++ src/features/f-registration/f-registration.vue
@@ -462,7 +462,7 @@ const handleTelegramConnect = async () => {
 const handleTelegramAuth = async () => {
   console.log(telegramBotName.value, telegramRedirectUrl.value);
   (window as any).Telegram.Login.auth(
-  { bot_id: telegramBotName.value, request_access: true },
+  { bot_id: '6841084234', request_access: true },
   (data) => {
     if (!data) {
       // authorization failed
