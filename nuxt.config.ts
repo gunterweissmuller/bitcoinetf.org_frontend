33d221f51e9544ca5652efd4e6891b87abc3c462
SweetRewenge <sweetrewenge@yandex.ru>
2024-03-23 04:29:04 +0500

    remove dbug and enable minify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ const stripScssMarkerLocal = '/* STYLES STRIP IMPORTS MARKER */'
 const stripScssMarker = process.env.NODE_ENV === 'production' ? stripScssMarkerProd : stripScssMarkerLocal
 export default defineNuxtConfig({
   ssr: false,
-  debug: true,
   app: {
     head: {
       title: 'BitcoinETF',
@@ -132,9 +131,6 @@ export default defineNuxtConfig({
   },
   css: ['@/src/app/styles/proxy-global.scss'],
   vite: {
-    build: {
-      minify: false,
-    },
     css: {
       preprocessorOptions: {
         scss: {
