26f8003e340c0470014bf75cddd413ea5afccb8d
Anton Zadorozniy <zadoroshniy_anton_2003@mail.ru>
2024-03-27 21:34:21 +0300

    revert

--- src/shared/ui/promolandings/landing-calculation-new/landing-calculation-new.vue
+++ src/shared/ui/promolandings/landing-calculation-new/landing-calculation-new.vue
@@ -54,7 +54,6 @@
             <nuxt-img src="/img/icons/colorful/telegram3.svg" class="landing-calculation__signup-buttons-item-img"></nuxt-img>
           </div>
           
-          <component :is="'script'" async src="https://telegram.org/js/telegram-widget.js?22" :data-telegram-login="telegramBotName" data-size="large" :data-auth-url="telegramRedirectUrl" data-request-access="write"></component>
         </div>
         <div class="landing-calculation__signup-line"></div>
       </div>
