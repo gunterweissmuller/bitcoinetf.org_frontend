365ff6f4e1c4326d0808c6212dea0dac22c468b3
Palwan Ashirov <palwan.ashirov@gmail.com>
2024-03-29 01:08:21 +0500

    update: changed styles to correct the main section on the referrals page

--- src/shared/ui/site/sections/s-site-referrals-main/s-site-referrals-main.vue
+++ src/shared/ui/site/sections/s-site-referrals-main/s-site-referrals-main.vue
@@ -1,23 +1,17 @@
 <template>
   <section class="s-site-referrals-main">
     <div class="s-site-referrals-main__img">
-      <img :src="data.img" alt="">
+      <img :src="data.img" alt="" />
     </div>
     <div class="l-site-dark">
       <div class="s-site-referrals-main__wrapper">
         <div class="s-site-referrals-main__left">
           <h1 v-html="data.title" class="s-site-referrals-main__title title-site-h1" />
-          <div class="s-site-referrals-main__message">
-            <div class="s-site-referrals-main__message-text">{{ data.message }}</div>
-            <div class="s-site-referrals-main__message-arrow">
-              <img src="/img/site-dark/s-site-referrals-main/arrow.svg" alt="">
-            </div>
-          </div>
         </div>
         <div class="s-site-referrals-main__right">
           <div class="s-site-referrals-main__avatars">
             <div class="s-site-referrals-main__avatar" v-for="(avatar, id) in data.avatars" :key="id">
-              <img :src="avatar" alt="">
+              <img :src="avatar" alt="" />
             </div>
           </div>
           <div class="s-site-referrals-main__text">{{ data.text }}</div>
