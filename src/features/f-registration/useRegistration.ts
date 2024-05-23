94de256079a302bdf6af54da1206fbb86162551b
Anton Zadorozniy <zadoroshniy_anton_2003@mail.ru>
2024-05-23 12:55:49 +0300

    comment facebook for production

--- src/features/f-registration/useRegistration.ts
+++ src/features/f-registration/useRegistration.ts
@@ -652,11 +652,11 @@ export function useRegistration($app) {
             img: $app.store.user.theme === 'dark' ? '/img/icons/colorful/apple.svg' : '/img/icons/mono/apple.svg',
             onClick: handleAppleConnect
         },
-        {
-            name: 'Facebook',
-            img: '/img/icons/colorful/facebook-circle.svg',
-            onClick: handleFacebookConnect
-        },
+        // {
+        //     name: 'Facebook',
+        //     img: '/img/icons/colorful/facebook-circle.svg',
+        //     onClick: handleFacebookConnect
+        // },
     ]
 
 
