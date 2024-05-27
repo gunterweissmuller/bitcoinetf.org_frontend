b8a3ccef2c713a7402862bb140b4dea1f5252cbc
Anton Zadorozniy <zadoroshniy_anton_2003@mail.ru>
2024-05-27 12:20:49 +0300

    wallet / sell etfs

--- src/widgets/w-etfs/w-etfs.vue
+++ src/widgets/w-etfs/w-etfs.vue
@@ -19,14 +19,14 @@
               Buy
           </div>
 
-          <!-- <div class="w-etfs__amount-buttons-item w-etfs__amount-buttons-item-secondary" @click="() => isShowSureModal = true">
+          <div v-if="$app.store.user.sellShares?.shares > 0" class="w-etfs__amount-buttons-item w-etfs__amount-buttons-item-secondary" @click="() => isShowSureModal = true">
             <a-icon
                 width="18"
                 height="18"
                 :name="Icon.MonoMinus"
               />
               Sell
-          </div> -->
+          </div>
 
           <div class="w-etfs__amount-buttons-item w-etfs__amount-buttons-item-secondary" @click="handleVerify">
             <a-icon
@@ -103,42 +103,25 @@
     </div>
   </div>
   <e-sell-etf-modal :show="isShowSureModal" :close="closeSureModal"  />
-  <f-withdrawal-modal :address="address" :method="method" v-model="isOpenModal" @accept="setMethod" />
-
   <!-- <w-onboarding :steps="renderedSteps" :next-route-name="nextRouteName" :is-purchase="nextRouteName == 'personal-buy-shares'"/> -->
 </template>
 
 <script setup lang="ts">
+import { useNuxtApp } from '#app'
 import AIcon from '~/src/shared/ui/atoms/a-icon/a-icon.vue'
 import { Icon } from '~/src/shared/constants/icons'
-import FWithdrawalModal from '~/src/features/f-withdrawal-modal/f-withdrawal-modal.vue'
 import eSellEtfModal from '~/src/entities/e-sell-etf-modal/e-sell-etf-modal.vue'
-import AButton from '~/src/shared/ui/atoms/a-button/a-button.vue'
 import { Centrifuge } from 'centrifuge'
 import { onUnmounted } from 'vue'
-import WOnboarding from '~/src/widgets/w-onboarding/w-onboarding.vue'
-import ALive from '~/src/shared/ui/atoms/a-live/a-live.vue'
 
 const { $app } = useNuxtApp()
 
-const isOpenModal = ref(false)
 const transactionsKey = ref(0)
-const orderType = computed(() => $app.store.user?.info?.account?.order_type || 'init_btc')
 
 const enum DIVIDENDS_TYPES {
   PLUS = 'credit_from_client',
   // MINUS = 'credit_from_client',
   ESCAPE = 'withdrawal',
-}
-
-const openModal = async () => {
-  const isKycFinished = await checkKyc()
-
-  if (isKycFinished) {
-    isOpenModal.value = true
-  } else {
-    navigateTo({ name: 'personal-kyc' })
-  }
 }
 
 const walletDividends = ref([])
@@ -147,61 +130,30 @@ const currentPage = ref(1)
 const hasNextPage = ref(true)
 const method = ref('')
 const address = ref('')
-const setMethodError = ref('')
-
 const centrifuge = ref(null)
-const withdrawalMethods = {
-  bitcoin_on_chain: 'On-chain',
-  bitcoin_lightning: 'Lightning',
-  polygon_usdt: 'Tether USDT (Polygon)',
-}
-const selectedWithdrawalMethod = computed(() => withdrawalMethods[method])
-const setMethod = async (value) => {
-  let methodType
-  if (value.method !== 'none') {
-    methodType = value.method
-  } else {
-    methodType = 'none'
-  }
-  await $app.api.info.billing
-    .setWithdrawalMethod({
-      address: value.address,
-      method: methodType,
-      walletType: 'dividends',
-    })
-    .then(async () => {
-      await getWalletDividends()
-    })
-    .catch((e) => {
-      if (e?.errors?.error?.message) {
-        setMethodError.value = e.errors.error.message
-      } else {
-        setMethodError.value = 'Something went wrong'
-      }
-    })
+
+const getSellEtfs = async () => {
+  $app.api.eth.billingEth
+  .initSellShares()
+  .then((response: any) => {
+    $app.store.user.sellShares = response.data
+  })
+  .catch(() => {
+    // Todo: notify something went wrond
+  })
 }
 
-const typeWorkMethod = computed(() => {
-  return (method.value === 'bitcoin_lightning' || method.value === 'polygon_usdt') ? 'Automatic' : 'Manual'
-})
-
-const typeMethodIcon = computed(() => {
-  if (method.value === 'bitcoin_lightning') {
-    return Icon.ColorfulBtcLightning
-  }
-
-  if (method.value === 'polygon_usdt') {
-    return Icon.ColorfulUsdtPolygon
-  }
-
-  return Icon.ColorfulBitcoin
-})
-
-const selectedAddressShort = computed(() => {
-  if (!address.value) return ''
-  if (address.value.length <= 5) return address.value
-  return address.value.slice(0, 5) + '...' + address.value.slice(address.value.length - 4)
-})
+const getValuate = async () => {
+  $app.api.eth.billingEth
+  .getValuate()
+  .then((response: any) => {
+    $app.store.user.sellShares = Object.assign($app.store.user.sellShares, response.data);
+    console.log($app.store.user.sellShares);
+  })
+  .catch(() => {
+    // Todo: notify something went wrond
+  })
+}
 
 const getLastPayment = async () => {
   $app.api.eth.billingEth
@@ -228,10 +180,6 @@ const getWalletDividends = async () => {
       // Todo: notify something went wrond
     })
 }
-
-const selectedMethod = computed(() => {
-  return method.value === 'bitcoin_on_chain' || method.value === 'bitcoin_lightning' || method.value === 'polygon_usdt'
-})
 
 const getDividendsDesc = (item) => {
   if (item.type === DIVIDENDS_TYPES.ESCAPE) {
@@ -290,34 +238,6 @@ const loadMoreDividends = async () => {
   await getPersonalDividends()
 }
 
-const usdAmount = computed(() => {
-  if (!walletDividends.value?.usd_amount) return 0
-
-  return $app.filters.rounded(walletDividends.value?.usd_amount, 2)
-})
-
-const btcAmount = computed(() => {
-  if (!walletDividends.value?.btc_amount) return 0
-
-  return $app.filters.convertValue($app.filters.rounded(walletDividends.value?.btc_amount, 8))
-})
-
-const isMore200Usd = computed(() => {
-  return walletDividends.value?.usd_amount > 100
-})
-
-
-
-const withdrawalDividends = async () => {
-  $app.api.eth.billingEth
-    .withdrawalDividends()
-    .then(() => {
-      getWalletDividends()
-    })
-    .catch((e) => {
-      console.error(e.errors)
-    })
-}
 const config = useRuntimeConfig()
 const centrifugeURL = config.public.WS_URL
 const centrifugeToken = config.public.WS_TOKEN
@@ -326,6 +246,8 @@ onMounted(async () => {
   await getWalletDividends()
   await getPersonalDividends()
   await getLastPayment()
+  await getSellEtfs()
+  await getValuate()
 
   centrifuge.value = new Centrifuge(centrifugeURL, {
     token: $app.store.auth.websocketToken ? $app.store.auth.websocketToken : centrifugeToken
@@ -341,6 +263,8 @@ onMounted(async () => {
         await getPersonalDividends(true)
         await getWalletDividends()
         await getLastPayment()
+        await getSellEtfs()
+        await getValuate()
       }, 1500)
     })
     .subscribe()
@@ -348,80 +272,6 @@ onMounted(async () => {
 
 onUnmounted(() => {
   centrifuge.value?.disconnect()
-})
-
-// Onboarding
-const { width } = useWindowSize()
-
-const journeySteps = computed(() => {
-  return [
-    {
-      attachTo: { element: width.value < 1024 ? '#menu-personal-dividends-bottom' : '#menu-personal-dividends' },
-      content: { title: 'Wallet', description: 'With BitcoinETF, manage three distinct digital wallets.' },
-      options: {
-        overlay: {
-          padding: 10,
-          borderRadius: 40,
-        },
-        popper: {
-          placement: width.value < 1024 ? 'top' : 'bottom',
-        },
-      },
-      on: {
-        beforeStep: async function (options) {
-          const elem = document.querySelector(options.step.attachTo.element)
-          elem?.classList.add('onboarding-index')
-        },
-        afterStep: async function (options) {
-          const elem = document.querySelector(options.step.attachTo.element)
-          elem?.classList.remove('onboarding-index')
-        },
-      },
-    },
-  ]
-})
-
-const steps = computed(() => {
-  return [
-    {
-      attachTo: { element: width.value < 1024 ? '#personal-dividends-tab-mobile' : '#personal-dividends-tab' },
-      content: {
-        title: 'Dividends Wallet',
-        description:
-          "This is your daily dividend destination, paid in Bitcoin. Convert to cash or retain as Bitcoin – we'll guide you through either choice.",
-      },
-      options: {
-        overlay: {
-          padding: 16,
-          borderRadius: 16,
-        },
-      },
-      on: {
-        beforeStep: async function (options) {
-          const elem = document.querySelector(options.step.attachTo.element)
-          elem?.classList.add('onboarding-index')
-        },
-        afterStep: function (options) {
-          const elem = document.querySelector(options.step.attachTo.element)
-          elem?.classList.remove('onboarding-index')
-        },
-      },
-    },
-  ]
-})
-
-const renderedSteps = computed(() => {
-  const journey = localStorage.getItem('journey')
-
-  if (!journey) {
-    return journeySteps.value.filter((step) => step.isRender !== false)
-  }
-
-  return steps.value.filter((step) => step.isRender !== false)
-})
-
-const nextRouteName = computed(() => {
-  return localStorage?.getItem('journey') ? 'personal-referrals' : 'personal-buy-shares'
 })
 
 // sure modal
@@ -432,34 +282,11 @@ const closeSureModal = () => {
 }
 
 // verify
-
-const checkKyc = async () => {
-  return await $app.api.eth.kyc.getForms().then((formsResponse: any) => {
-    return formsResponse.data[0].status === 'passed'
-  })
-}
-
 const explorerURL = config.public.EXPLORER_API;
 const explorerHostname = `https://${explorerURL}`;
 
 const handleVerify = async () => {
   window.open(`${explorerHostname}/account/${$app.store.user?.blockchainUserWallet}`, '_blank')
-  // const isKycFinished = await checkKyc();
-
-  // window.open(window.location.origin + "/personal/kyc");
-
-  // navigateTo({ name: 'personal-kyc' }, {
-  //   open : {
-  //     target : "_blank"
-  //   }
-  // });
-
-  // if (isKycFinished) {
-  //   //todo ?
-  //   navigateTo({ name: 'personal-kyc' })
-  // } else {
-  //   navigateTo({ name: 'personal-kyc' })
-  // }
 }
 
 </script>
